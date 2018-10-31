0b1828b037ab6ebd4174c3760efeee11da5a02ac
Luis Gonzalez <luisarmando1234@gmail.com>
2018-10-31 14:45:41 -0600

    Create FacturaReporte.php

---
+++ src/models/FacturaReporte.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gogl92
+ * Date: 31/10/18
+ * Time: 12:30 PM
+ */
+
+namespace inquid\facturacom\models;
+use yii\base\Model;
+
+class FacturaReporte extends Model
+{
+    public $RazonSocialReceptor;
+}
